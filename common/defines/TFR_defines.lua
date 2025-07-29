d85ad871f2f5410c2becc0ce8f95f35aa1527509
Mihai <mihaihd5ro@gmail.com>
2025-07-30 01:29:08 +0300

    TFB OPTIMIZATION

---
+++ common/defines/TFR_defines.lua
@@ -0,0 +1,12 @@
+NDefines.NGame.GAME_SPEED_SECONDS = { 1000.0, 0.19, 0.10, 0.06, 0.0 } 
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 999				-- Days of client lag for decrease of gamespeed
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 999				-- Days of client lag for pause of gamespeed.
+
+NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 12
+NDefines.NGame.EVENT_TIMEOUT_DEFAULT = 15						-- Default days before an event times out if not scripted
+NDefines.NGame.MISSION_REMOVE_FROM_INTERFACE_DEFAULT = 15		-- Default days before a mission is removed from the interface after having failed or completed
+NDefines.NGame.HANDS_OFF_START_TAG = "VEN"
+NDefines.NGame.ALERT_SFX_COOLDOWN_DAYS = 3					-- After playing an alert sound, don't play the same sound for XXX days, even if it fires again.
+NDefines.NGame.TRADE_ROUTE_RECALCULATE_FREQUENCY_DAYS = 7 -- Max recalculation time for all trade routes (0 means we do not recalucate prediodically trade routes)
+
+

8a7a180777f87373605c7b67f6b89b25bdeb1533
Mihai <mihaihd5ro@gmail.com>
2025-07-30 17:16:32 +0300

    more fixes

--- common/defines/TFR_defines.lua
+++ common/defines/TFR_defines.lua
@@ -16,8 +16,19 @@ NDefines.NGeography.MEDITERRANEAN_SEA_REGIONS = { 15, 29, 68, 69, 168, 169, 202,
 
 NDefines.NProduction.BASE_LICENSE_IC_COST = 0
 NDefines.NProduction.LICENSE_IC_COST_YEAR_INCREASE = 0
+NDefines.NProduction.LICENSE_EQUIPMENT_BASE_SPEED = 0
+NDefines.NProduction.LICENSE_EQUIPMENT_TECH_SPEED_PER_YEAR = 0
+NDefines.NProduction.LICENSE_EQUIPMENT_TECH_SPEED_MAX_YEARS = 4
+NDefines.NProduction.LICENSE_EQUIPMENT_SPEED_NOT_FACTION = 0
 NDefines.NProduction.LICENSE_EQUIPMENT_UPGRADE_XP_FACTOR = 1.0
 NDefines.NProduction.LICENSE_EQUIPMENT_SPEED_NO_LICENSE = 0
+NDefines.NProduction.CONVERSION_SPEED_BONUS = 0
+NDefines.NProduction.EQUIPMENT_MODULE_ADD_XP_COST = 5.0
+NDefines.NProduction.EQUIPMENT_MODULE_REPLACE_XP_COST = 2.0				-- XP cost for replacing one equipment module with an unrelated module when creating an equipment variant.
+NDefines.NProduction.EQUIPMENT_MODULE_CONVERT_XP_COST = 1.0				-- XP cost for converting one equipment module to a related module when creating an equipment variant.
+NDefines.NProduction.EQUIPMENT_MODULE_REMOVE_XP_COST = 1.0
+		
+
 
 NDefines.NTechnology.BASE_RESEARCH_POINTS_SAVED = 50.0
 NDefines.NTechnology.BASE_YEAR_AHEAD_PENALTY_FACTOR = 3
@@ -30,3 +41,14 @@ NDefines.NCountry.MAX_COMMAND_POWER = 120.0						-- Max command power a country 
 NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 30
 
 NDefines.NIndustrialOrganisation.ASSIGN_DESIGN_TEAM_PP_COST_PER_DAY = 0.0
+
+NDefines.NMilitary.RELIABILTY_RECOVERY = 0.1
+NDefines.NMilitary.ATTRITION_EQUIPMENT_LOSS_CHANCE = 0.2
+NDefines.NMilitary.TRAINING_ATTRITION = 0.00
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 999
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 999
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 999
+NDefines.NMilitary.UNIT_LEADER_USE_NONLINEAR_XP_GAIN = false
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 3 	--Base cost to unlock a regiment slot
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 2	--Base cost to change a regiment column.
+NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 2 	--Base cost to unlock a support slot

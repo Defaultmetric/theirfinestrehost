0a29cc2a9e4c94974f8a1c5d9243cda0bb800b08
theretardhimself <gyima07@gmail.com>
2026-03-04 00:59:53 +0100

    big boom (double check before game)
    nerf, nerf, nerf ig

--- common/defines/TFR_defines.lua
+++ common/defines/TFR_defines.lua
@@ -65,6 +65,7 @@ NDefines.NMilitary.LAND_AIR_COMBAT_STR_DAMAGE_MODIFIER = 0.030    -- air global 
 NDefines.NMilitary.LAND_COMBAT_STR_DAMAGE_MODIFIER = 0.060        -- global damage modifier... but some equipment is returned at end of battles see : EQUIPMENT_COMBAT_LOSS_FACTOR
 NDefines.NMilitary.LAND_COMBAT_ORG_DAMAGE_MODIFIER = 0.045        -- global damage modifier
 NDefines.NMilitary.COMBAT_STACKING_PENALTY = -0.05                -- how much stackign penalty per division
+NDefines.NMilitary.COMBAT_OVER_WIDTH_PENALTY = -2.5				-- how much penalty for each width over the limit (so if you have 10 width and the limit is 7, you get 3*3=9 penalty)
 NDefines.NMilitary.PIERCING_THRESHOLDS = {					-- Our piercing / their armor must be this value to deal damage fraction equal to the index in the array below [higher number = higher penetration]. If armor is 0, 1.00 will be returned.
 	1.00,
 	0.9,
